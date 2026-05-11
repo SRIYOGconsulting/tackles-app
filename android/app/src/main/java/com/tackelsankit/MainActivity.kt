8b596af39d0b3f85f99e8accc968bfc02ba6853e
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2026-05-11 21:16:22 +0545

    update3

--- android/app/src/main/java/com/tackelsankit/MainActivity.kt
+++ android/app/src/main/java/com/tackelsankit/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tackelsankit
+package com.tacklesankit
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -14,7 +14,7 @@ override fun onCreate(savedInstanceState: Bundle?) {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "TackelsAnkit"
+  override fun getMainComponentName(): String = "TacklesAnkit"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/tackelsankit/MainApplication.kt
+++ android/app/src/main/java/com/tackelsankit/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.tackelsankit
+package com.tacklesankit
 
 import android.app.Application
 import com.facebook.react.PackageList
